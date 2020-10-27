ae12e1fb83cb0e222fcd8719e048dc7504af56b3
Nammu <mod157@naver.com>
2020-10-27 18:02:33 +0900

    Test Login

---
+++ Tower_Defense/Assets/TowerDefense/Scripts/Scene/TowerSceneManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TowerSceneManager : MonoBehaviour
+{
+
+   private AsyncOperation oper;
+   public static TowerSceneManager _Instance;
+
+   public static TowerSceneManager instance
+    {
+        get
+        {
+            if (_Instance == null)
+            {
+                // This is where the magic happens.
+                //  FindObjectOfType(...) returns the first AManager object in the scene.
+
+                _Instance = FindObjectOfType(typeof(TowerSceneManager)) as TowerSceneManager;
+            }
+
+            // If it is still null, create a new instance
+            if (_Instance == null)
+            {
+                GameObject obj = new GameObject("SceneManager");
+                _Instance = obj.AddComponent(typeof(TowerSceneManager)) as TowerSceneManager;
+            }
+
+            return _Instance;
+        }
+    }
+
+    private void Awake() {
+        DontDestroyOnLoad(this);
+    }
+
+    IEnumerator ReadyLoadScene(string name)
+    {
+        oper = SceneManager.LoadSceneAsync(name);
+        oper.allowSceneActivation = false;//true 되면 다음으로 넘어감
+
+        yield return null;
+    }
+
+   public void LoadScene(string name){
+
+       switch(name){
+            case "Login" : 
+                SceneManager.LoadSceneAsync("LoginScene");
+                break;
+
+            case "Menu" :
+                SceneManager.LoadSceneAsync("MenuScene");
+                break;
+
+            case "Game" :
+                SceneManager.LoadSceneAsync("GameScene");
+                break;
+       }
+
+   }
+   
+}
