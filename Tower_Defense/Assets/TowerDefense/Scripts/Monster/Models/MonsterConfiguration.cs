db67ccc5435f88843ba93114afe395ffe57e5f09
Nammu <nammu8395@gmail.com>
2020-10-28 00:14:58 +0900

    Add MonsterScript

---
+++ Tower_Defense/Assets/TowerDefense/Scripts/Monster/Models/MonsterConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Monster
+{  
+    abstract public class MonsterConfiguration
+    {
+        private MonsterType _Type ;
+
+        private int _HP { get; set; }
+
+        private double _MoveSpeed { get; set; } 
+
+        private int _Coin { get; set; } 
+    }
+}
