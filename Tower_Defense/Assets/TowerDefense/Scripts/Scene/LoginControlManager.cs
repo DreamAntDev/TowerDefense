1aca6333bbe1eae2b00d0d18d40a37f8e031b0a7
hojin-kr <jhj377@gmail.com>
2021-02-02 01:03:50 +0900

    구글 로그인 / 게임서버 로그인 각각 구현

--- Tower_Defense/Assets/TowerDefense/Scripts/Scene/LoginControlManager.cs
+++ Tower_Defense/Assets/TowerDefense/Scripts/Scene/LoginControlManager.cs
@@ -1,13 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
+using System.Text;
 
-public class LoginControlManager : MonoBehaviour
-{
+public class LoginControlManager : MonoBehaviour {
     Firebase.Auth.FirebaseAuth auth;    
     public string email;
     public string password;
-    
     // Start is called before the first frame update
     void Start()
     { 
@@ -16,11 +16,11 @@ public class LoginControlManager : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetMouseButtonDown(0))
-        {
+        if (Input.GetMouseButtonDown(0)) {
             SignIn();
         }
     }
+    // TODO google login
     void SignUp() {
         auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
         if (task.IsCanceled) {
@@ -31,7 +31,6 @@ public class LoginControlManager : MonoBehaviour
             Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
             return;
         }
-
         // Firebase user has been created.
         Firebase.Auth.FirebaseUser newUser = task.Result;
         Debug.LogFormat("Firebase user created successfully: {0} ({1})",
@@ -49,10 +48,24 @@ public class LoginControlManager : MonoBehaviour
             Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
             return;
         }
-
         Firebase.Auth.FirebaseUser newUser = task.Result;
-        Debug.LogFormat("User signed in successfully: {0} ({1})",
-            newUser.DisplayName, newUser.UserId);
+        Debug.LogFormat("User signed in successfully: {0}", newUser.UserId);
+        Debug.Log(newUser.UserId);
         });
+    }
+
+    IEnumerator loginGameServer(string PlatformID) {
+        // TODO 구조화 google auth 연결
+        string data = "{'platform_id':'" + PlatformID + "','platform':'google','device_id':'" + SystemInfo.deviceUniqueIdentifier + "'}";
+        UnityWebRequest webRequest = UnityWebRequest.Post("http://3.36.40.68:8888/login", data);
+        webRequest.uploadHandler = new UploadHandlerRaw(Encoding.UTF8.GetBytes(data));
+        webRequest.SetRequestHeader("Content-Type", "application/json");
+        yield return webRequest.SendWebRequest();
+        if(webRequest.isNetworkError || webRequest.isHttpError) {
+            Debug.Log(webRequest.error);
+        } else {
+            Debug.Log(webRequest.downloadHandler.text);
+            Debug.Log("login complete! ");
+        }
     }
 }
