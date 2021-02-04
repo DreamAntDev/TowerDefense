7e2ce87700c74ce6daa094b8cff552127defe926
KwakDongKyu <dongkyu0608@gmail.com>
2021-02-04 23:02:27 +0900

    리소스관리구조 수정

---
+++ Tower_Defense/Assets/TowerDefense/Scripts/Initializer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Initializer : SingletonBehaviour<Initializer>
+{
+    public List<GameObject> gameObjects;
+    private new void Awake()
+    {
+        base.Awake();
+        foreach(var obj in gameObjects)
+        {
+            if(obj != null)
+                Instantiate(obj);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
